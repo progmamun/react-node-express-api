af777328cbdc3d19c129983614b7fabb79c47b95
progmamun <mamun333089@gmail.com>
2022-04-20 15:47:52 +0600

    fetch-data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>My Own Data: {users.length}</h2>
+      {users.map(user => (
+        <li key={user.id}>
+          Name:{user.name} Mail: {user.email}
+        </li>
+      ))}
     </div>
   );
 }
